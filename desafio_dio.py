8781b88d734338a1e60c7ab152bfd81f473cc862
Anderson Santos da Silva <62861635+halozer01@users.noreply.github.com>
2024-06-09 00:44:14 -0300

    Update desafio_dio.py

--- desafio_dio.py
+++ desafio_dio.py
@@ -1,1 +1,67 @@
+def menu():
+    return """
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
 
+    => """
+
+def depositar(saldo, extrato):
+    valor = float(input("Informe o valor do depósito: "))
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+        print("Depósito realizado com sucesso!")
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+    return saldo, extrato
+
+def sacar(saldo, extrato, numero_saques, LIMITE_SAQUES, limite):
+    valor = float(input("Informe o valor do saque: "))
+
+    if valor <= 0:
+        print("Operação falhou! O valor informado é inválido.")
+    elif valor > saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif valor > limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif numero_saques >= LIMITE_SAQUES:
+        print("Operação falhou! Número máximo de saques excedido.")
+    else:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+        print("Saque realizado com sucesso!")
+    
+    return saldo, extrato, numero_saques
+
+def exibir_extrato(saldo, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+
+    while True:
+        opcao = input(menu())
+
+        if opcao == "d":
+            saldo, extrato = depositar(saldo, extrato)
+        elif opcao == "s":
+            saldo, extrato, numero_saques = sacar(saldo, extrato, numero_saques, LIMITE_SAQUES, limite)
+        elif opcao == "e":
+            exibir_extrato(saldo, extrato)
+        elif opcao == "q":
+            break
+        else:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
+
+if __name__ == "__main__":
+    main()
